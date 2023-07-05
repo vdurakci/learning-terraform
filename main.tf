09d31fa583d90e645d730798613815d8b389f8a8
Volkan Durakci <volkan.durakci@gmail.com>
2023-07-05 19:38:01 -0400

    rollback to previous version

--- main.tf
+++ main.tf
@@ -14,21 +14,8 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+data "aws_vpc" "default" {
+  default = true
 }
 
 resource "aws_instance" "blog" {
@@ -49,7 +36,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog"
 
-  vpc_id              = module.vpc.public_subnets[0]
+  vpc_id              = data.aws_vpc.default.id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
